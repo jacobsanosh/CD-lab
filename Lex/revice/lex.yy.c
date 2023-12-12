3d8f7a5213722dcc3887c6cef1bb1e0723d1a0a5
jacobsanosh <jacobsanosh@gmail.com>
2023-12-12 16:54:57 +0530

    m

--- Lex/revice/lex.yy.c
+++ Lex/revice/lex.yy.c
@@ -360,19 +360,19 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[7] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    4,    1,    2,    0
+        0,    0,    4,    3,    2,    3,    2,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    2,    1,    1,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +397,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[3] =
+static const YY_CHAR yy_meta[4] =
     {   0,
-        1,    1
+        1,    1,    2
     } ;
 
-static const flex_int16_t yy_base[7] =
+static const flex_int16_t yy_base[13] =
     {   0,
-        0,    0,    3,    4,    4,    4
+        6,    0,    8,   10,    0,    0,    0,    0,   10,    3,
+        5,    4
     } ;
 
-static const flex_int16_t yy_def[7] =
+static const flex_int16_t yy_def[13] =
     {   0,
-        6,    1,    6,    6,    6,    0
+       10,    9,    9,    9,   11,   12,   11,   12,    0,    9,
+        9,    9
     } ;
 
-static const flex_int16_t yy_nxt[7] =
+static const flex_int16_t yy_nxt[14] =
     {   0,
-        4,    5,    6,    3,    6,    6
+        4,    6,    5,    4,    4,    8,    7,    9,    5,    3,
+        9,    9,    9
     } ;
 
-static const flex_int16_t yy_chk[7] =
+static const flex_int16_t yy_chk[14] =
     {   0,
-        1,    1,    3,    6,    6,    6
+        2,    2,    2,   10,   10,   12,   11,    3,    1,    9,
+        9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,13 +440,13 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "no_of_letters.l"
-#line 2 "no_of_letters.l"
-#include<stdio.h>
-#include<string.h>
-int i=0;
-#line 445 "lex.yy.c"
-#line 446 "lex.yy.c"
+#line 1 "pos_neg.l"
+#line 2 "pos_neg.l"
+        #include<stdio.h>
+        #include<stdlib.h>
+        int pos=0,neg=0;
+#line 449 "lex.yy.c"
+#line 450 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -622,6 +626,9 @@ extern int yylex (void);
 #endif
 
 #define YY_RULE_SETUP \
+	if ( yyleng > 0 ) \
+		YY_CURRENT_BUFFER_LVALUE->yy_at_bol = \
+				(yytext[yyleng - 1] == '\n'); \
 	YY_USER_ACTION
 
 /** The main scanner function which does all the work.
@@ -659,9 +666,9 @@ YY_DECL
 		}
 
 	{
-#line 7 "no_of_letters.l"
+#line 6 "pos_neg.l"
 
-#line 665 "lex.yy.c"
+#line 672 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -676,6 +683,7 @@ YY_DECL
 		yy_bp = yy_cp;
 
 		yy_current_state = (yy_start);
+		yy_current_state += YY_AT_BOL();
 yy_match:
 		do
 			{
@@ -688,13 +696,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 7 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 4 );
+		while ( yy_base[yy_current_state] != 10 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -720,21 +728,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 "no_of_letters.l"
-{i++;}
+#line 7 "pos_neg.l"
+{neg++;printf("\nnegvalue =%s",yytext);}
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 9 "no_of_letters.l"
-{printf("%d\n",i);i=0;}
+#line 8 "pos_neg.l"
+{pos++;printf("\nposvalue =%s",yytext);}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 10 "no_of_letters.l"
+#line 9 "pos_neg.l"
 ECHO;
 	YY_BREAK
-#line 738 "lex.yy.c"
+#line 745 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1019,6 +1026,7 @@ static int yy_get_next_buffer (void)
 	char *yy_cp;
     
 	yy_current_state = (yy_start);
+	yy_current_state += YY_AT_BOL();
 
 	for ( yy_cp = (yytext_ptr) + YY_MORE_ADJ; yy_cp < (yy_c_buf_p); ++yy_cp )
 		{
@@ -1031,7 +1039,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 7 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1059,11 +1067,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 7 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 6);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1178,6 +1186,8 @@ static int yy_get_next_buffer (void)
 	c = *(unsigned char *) (yy_c_buf_p);	/* cast for 8-bit char's */
 	*(yy_c_buf_p) = '\0';	/* preserve yytext */
 	(yy_hold_char) = *++(yy_c_buf_p);
+
+	YY_CURRENT_BUFFER_LVALUE->yy_at_bol = (c == '\n');
 
 	return c;
 }
@@ -1739,13 +1749,14 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 10 "no_of_letters.l"
-
+#line 9 "pos_neg.l"
 
 int yywrap(){return 1;}
-
-int main(){
-	yylex();
-	return 0;
+int main()
+{
+        printf("\nenter the numbers\t");
+        yylex();
+        printf("\n pos =%d neg=%d",pos,neg);
+        return 0;
 }
 
